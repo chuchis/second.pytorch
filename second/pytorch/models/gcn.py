a75bf6875e3678c2d1c5e73d8f90ed27414f9830
Jesus Zarzar <jesusalejandro.zarzartorano@kaust.edu.sa>
2019-09-22 21:59:46 +0300

    Added repeat for feature mask

--- second/pytorch/models/gcn.py
+++ second/pytorch/models/gcn.py
@@ -195,8 +195,9 @@ class PillarGCNFeatureNet(nn.Module):
         # Forward pass through PFNLayers
         for pfn in self.pfn_layers:
             # print(features.shape)
-            features -= (1-mask)*1e10
+            features -= (1-mask.repeat(1,features.shape[1],1))*1e10
             features = batch_process(features, pfn, num_batches=20)
-            features *= mask
+            features *= mask.repeat(1,features.shape[1],1)
+            
             # print(features.shape)
         return features.squeeze()
