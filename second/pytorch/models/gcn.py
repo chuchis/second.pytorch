cea17d2f9c382e6f68b9ce6028352f6c1c5bffe2
Jesus Zarzar <jesusalejandro.zarzartorano@kaust.edu.sa>
2019-09-28 17:37:40 +0300

    Added residual connection function

--- second/pytorch/models/gcn.py
+++ second/pytorch/models/gcn.py
@@ -111,6 +111,103 @@ class GCNLayer(nn.Module):
             x_concatenated = torch.cat([x, x_repeat], dim=2)
             return x_concatenated
 
+@register_vfe
+class DeepGCNFeatureNet(nn.Module):
+    def __init__(self,
+                 num_input_features=4,
+                 use_norm=True,
+                 num_filters=(64, ),
+                 with_distance=False,
+                 voxel_size=(0.2, 0.2, 4),
+                 pc_range=(0, -40, -3, 70.4, 40, 1)):
+        """
+        Pillar Feature Net.
+        The network prepares the pillar features and performs forward pass through PFNLayers. This net performs a
+        similar role to SECOND's second.pytorch.voxelnet.VoxelFeatureExtractor.
+        :param num_input_features: <int>. Number of input features, either x, y, z or x, y, z, r.
+        :param use_norm: <bool>. Whether to include BatchNorm.
+        :param num_filters: (<int>: N). Number of features in each of the N PFNLayers.
+        :param with_distance: <bool>. Whether to include Euclidean distance to points.
+        :param voxel_size: (<float>: 3). Size of voxels, only utilize x and y size.
+        :param pc_range: (<float>: 6). Point cloud range, only utilize x and y min.
+        """
+
+        super().__init__()
+        self.name = 'DeepGCNFeatureNet'
+        assert len(num_filters) > 0
+        num_input_features += 5
+        if with_distance:
+            num_input_features += 1
+        self._with_distance = with_distance
+
+        # Create PillarFeatureNet layers
+        num_filters = [num_input_features] + list(num_filters)
+        pfn_layers = []
+        for i in range(len(num_filters) - 1):
+            in_filters = num_filters[i]
+            out_filters = num_filters[i + 1]
+            if i < len(num_filters) - 2:
+                last_layer = False
+            else:
+                last_layer = True
+            pfn_layers.append(
+                GCNLayer(
+                    in_filters*2, out_filters, use_norm, last_layer=last_layer))
+        self.pfn_layers = nn.ModuleList(pfn_layers)
+
+        # Need pillar (voxel) size and x/y offset in order to calculate pillar offset
+        self.vx = voxel_size[0]
+        self.vy = voxel_size[1]
+        self.x_offset = self.vx / 2 + pc_range[0]
+        self.y_offset = self.vy / 2 + pc_range[1]
+
+    def forward(self, features, num_voxels, coors):
+        # print(torch.sum(num_voxels)/2)
+        device = features.device
+
+        dtype = features.dtype
+        # print(features.shape, num_voxels.shape, coors.shape)
+        # print(coors[0])
+        # print(num_voxels[0])
+        # Find distance of x, y, and z from cluster center
+        points_mean = features[:, :, :3].sum(
+            dim=1, keepdim=True) / num_voxels.type_as(features).view(-1, 1, 1)
+        f_cluster = features[:, :, :3] - points_mean
+
+        # Find distance of x, y, and z from pillar center
+        f_center = torch.zeros_like(features[:, :, :2])
+        f_center[:, :, 0] = features[:, :, 0] - (
+            coors[:, 3].to(dtype).unsqueeze(1) * self.vx + self.x_offset)
+        f_center[:, :, 1] = features[:, :, 1] - (
+            coors[:, 2].to(dtype).unsqueeze(1) * self.vy + self.y_offset)
+
+        # Combine together feature decorations
+        features_ls = [features, f_cluster, f_center]
+        if self._with_distance:
+            points_dist = torch.norm(features[:, :, :3], 2, 2, keepdim=True)
+            features_ls.append(points_dist)
+        features_out = torch.cat(features_ls, dim=-1)
+
+        # The feature decorations were calculated without regard to whether pillar was empty. Need to ensure that
+        # empty pillars remain set to zeros.
+        voxel_count = features_out.shape[1]
+        mask = get_paddings_indicator(num_voxels, voxel_count, axis=0)
+        mask = torch.unsqueeze(mask, -1).type_as(features_out)
+        # print(torch.sum(features[:,:,:3] - (features*mask)[:,:,:3]))
+        features_out = features_out * mask
+		prev_features_out = torch.zeros_like(features_out)
+        # Forward pass through PFNLayers
+        for pfn in self.pfn_layers:
+            features_out = batch_process(features_out+prev_features_out, pfn, num_batches=10)
+            features_out = features_out * mask
+			prev_features_out = features_out
+            features_max = torch.max(features_out, dim=1, keepdim=True)[0]
+            if pfn.last_vfe:
+                features_out = features_max
+            else:
+                features_repeat = features_max.repeat(1, features_out.shape[1], 1)
+                features_out = torch.cat([features_out, features_repeat], dim=2)
+        return features_out.squeeze() 
 
 @register_vfe
 class PillarGCNFeatureNet(nn.Module):
